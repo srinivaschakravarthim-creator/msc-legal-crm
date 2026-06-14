39ac5e5d604e90b0688d456fa5648ad0c8ee26da
srinivaschakravarthim-creator <srinivaschakravarthim@gmail.com>
2026-06-15 00:08:05 +0530

    Delete deploy-upload directory

--- deploy-upload/supabase/seed_firm.sql
+++
@@ -1,6 +0,0 @@
--- Initial firm record for MSC Legal CRM
--- Run this after supabase/schema.sql.
-
-insert into firms (name, city, state, gstin)
-values ('MSC Legal CRM', 'Visakhapatnam', 'Andhra Pradesh', null)
-returning id, name, city, state, gstin;
